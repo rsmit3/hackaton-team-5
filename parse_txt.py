6e5f8218616053a6406df3189b1d4105087a10c9
Tom <tuijtdehaag@heijmans.nl>
2021-11-05 12:16:40 +0100

    Parser for lidar output txt

---
+++ parse_txt.py
@@ -0,0 +1,36 @@
+import json
+
+def parse(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    lines = [line.strip().split(' ') for line in lines]
+
+    frame = []
+    S_index = []
+    for line_index, line in enumerate(lines):
+        line_dict = {}
+        for i in range(0, len(line), 2):
+            k, v = line[i].strip(':'), line[i+1]
+
+            if k == 'S':
+                S_index.append(line_index)
+                continue
+
+            line_dict[k] = float(v)
+
+        frame.append(line_dict)
+
+    json_out = []
+    for i in range(len(S_index) - 1):
+        rotation = frame[S_index[i]:S_index[i + 1]]
+        json_out.append(rotation)
+
+    return json_out
+
+
+if __name__ == '__main__':
+    data = parse('data/LidarData.txt')
+
+    with open('data/LidarData.json', 'w') as f:
+        f.write(json.dumps(data, indent=2))
